edc7a2b8201e239b59d8cbe90884c2f38a356754
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-05-15 23:23:52 +0300

    New design

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {CloudUpload} from "@mui/icons-material"
+import { Button,FormControl ,InputLabel, MenuItem, Select, TextField, Typography, Checkbox, FormGroup, FormControlLabel, FormLabel, RadioGroup, Radio, Fab, styled, Grid, Divider } from '@mui/material';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 function App() {
   const proffesions = [
-    '',
     'Программист',
     'Тестировщик',
     'Аналитик'
@@ -13,36 +26,55 @@ function App() {
 
   return (
     <>
-      <h1>Форма</h1>
+      <Typography variant='h1' component="h1">Форма</Typography>
+      <br />
       <form action="" encType="multipart/form-data">
-        <p>ФИО</p>
-        <input type="text" name="FIO" maxLength="35"/>
-        <p>Почта</p>
-        <input type="email" name="Mail" id=""/>
-        <p>Пароль для ЛК</p>
-        <input type="password" name="" id=""/>
-        <p>Биография</p>
-        <textarea name="BIO" id="" rows="5"></textarea>
-        <p>Желаемая вакансия</p>
-        <select name="prof" id="">
+        <TextField label="ФИО" variant='outlined' color='primary'/> <br /> <br />
+        <TextField label="Почта" variant='outlined' color='primary'/> <br /><br />
+        <TextField label="Пароль для ЛК" variant='outlined' color='primary' type='password'/> <br /><br />
+        <TextField label="Биография" variant='outlined' color='primary' rows="5" multiline/> <br /><br />
+        <FormControl fullWidth>
+          <InputLabel>Желаемая вакансия</InputLabel>
+        <Select label="Желаемая вакансия" >
           {proffesions.map((pr,ind)=>
-            <option value={ind}>{pr}</option>
+            <MenuItem value={ind}>{pr}</MenuItem>
           )}
-        </select>
-        <p>Владение языками</p>
-        <input type="checkbox" name="" id="C"/>С# <br/><input type="checkbox" name="" id="CPP"/>C++ <br/><input type="checkbox" name="" id="JS"/>JS <br/><input type="checkbox" name="" id="PY"/>Python
-        <br/>
-        <p>Дополнительный навык:</p>
-        <input type="radio" name="" id=""/>Владение иностранным языком  <br/>
-        <input type="radio" name="" id=""/>Безошибочность <br/>
-        <input type="radio" name="" id=""/>Коммуникативность  <br/><br/>
-        <p>Приложите документы</p>
-        <input type="file" name="" id=""/>
-        <br/>
-        <input type="image" src="a.jpg" width="200"/>
-        <br/><br/>
-        <input type="reset" value="Очистить форму"/>
-        <input type="submit" value="Отправить"/>
+        </Select> <br />
+        </FormControl>
+        <FormGroup>
+          <InputLabel>Владение языками</InputLabel>
+          <FormControlLabel control={<Checkbox/>} label="C#"/>
+          <FormControlLabel control={<Checkbox/>} label="C++"/>
+          <FormControlLabel control={<Checkbox/>} label="JavaScript"/>
+        </FormGroup> <br />
+        <FormControl>
+          <FormLabel>Дополнительный навык:</FormLabel>
+          <RadioGroup>
+            <FormControlLabel value="0" control={<Radio/>} label="Владение иностранным языком"/>
+            <FormControlLabel value="1" control={<Radio/>} label="Безошибочность"/>
+            <FormControlLabel value="2" control={<Radio/>} label="Коммуникативность"/>
+          </RadioGroup>
+        </FormControl>
+        <br /><br />
+        <Button
+  component="label"
+  role={undefined}
+  variant="contained"
+  tabIndex={-1}
+  startIcon={<CloudUpload/>}
+>
+  Upload files
+  <VisuallyHiddenInput
+    type="file"
+    onChange={(event) => console.log(event.target.files)}
+    multiple
+  />
+</Button>
+<Divider sx={{my: 3}}/>
+          <Grid container spacing={2}>
+              <Button color='warning' variant='outlined'>Очистить форму</Button>
+              <Button variant='contained'>Отправить</Button>
+          </Grid>
         <br/><br/>
         <input type="hidden" name="something" value="Скрытое значение"/>
     </form>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider, createTheme } from '@mui/material'
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
